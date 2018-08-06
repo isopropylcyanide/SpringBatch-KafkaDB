1582fd2d085d611ec9cd82c35f268ca046f66c3f
Aman Garg <amangargcse@outlook.com>
2018-08-06 17:35:22 +0530

    Initial scaffold

---
+++ src/main/java/com/accolite/aman/SpringBatchCsvProcessor/SpringBatchCsvProcessorApplication.java
@@ -0,0 +1,12 @@
+package com.accolite.aman.SpringBatchCsvProcessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchCsvProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchCsvProcessorApplication.class, args);
+	}
+}
