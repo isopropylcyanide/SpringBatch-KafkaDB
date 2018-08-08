6b9e6e18dab790804b9850eb38491b448c77b2e2
Aman Garg <amangargcse@outlook.com>
2018-08-08 12:11:21 +0530

    Remove unnecessary scheduling

--- src/main/java/com/accolite/aman/SpringBatchCsvProcessor/config/SpringBatchConfig.java
+++ src/main/java/com/accolite/aman/SpringBatchCsvProcessor/config/SpringBatchConfig.java
@@ -15,11 +15,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableBatchProcessing
-@EnableScheduling
 public class SpringBatchConfig {
 
 	@Value("${input.csv.file}")
